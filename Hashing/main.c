d911a763edc07d892ec096296077a8d033c9e5b1
w-josh <webj106@outlook.com>
2024-10-23 00:15:30 +1000

    Hash display method and notes implemented

--- Hashing/main.c
+++ Hashing/main.c
@@ -53,3 +53,35 @@ void insert(int key, int value) {
         hashTable[index] = newNode;
     }
 }
+
+//The conditional Operator explained for collisions:
+
+//The if statement checks if the current index is empty. If it is, the newNode is inserted at that location/index.
+//if there is already data/node at the index, then a collision occurs. Therefore, these steps happen:
+// STEP 1: The newNode next pointer will be set to point to the current hashTable index that it's colliding with.
+//By doing this, the current Node data at that collided location is kept safely in the next pointer of the newNode
+//STEP 2: Now newNode can replace the current node, whilst holding the previous nodes data in its next pointer.
+
+void display() {
+    for(int i = 0; i < TABLE_SIZE; i++) {
+        Node* temp = hashTable[i];
+        printf("index %d:", i);
+        while(temp != NULL) {
+            printf("%d, %d -> ", temp->key, temp->value);
+            temp = temp->next;
+        }
+    }
+}
+//why use temp?
+//temp variable assignment prevents original linked list node data from being lost
+//if you were to use hashTable[0] = hashTable[0]->next, you would be assigning the next pointer to null at the end of the list iteration.
+//This would mean you'd lose all references of the linked list at that index after iteration is complete.
+//Example: hashTable[0] -> [Node1: key=1, value=100, next=Node2] -> [Node2: key=2, value=200, next=Node3] -> [Node3: key=3, value=300, next=NULL]
+//In the example above, the hashTable will iterate and print each Node. However once the end is reached, the Node next pointer at that index will be replaced with null.
+//This results in all node references at that index being lost.
+
+
+
+int search(int key) {
+
+}
